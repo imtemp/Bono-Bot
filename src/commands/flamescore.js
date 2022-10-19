4af0202fc353cea08f2633b081edc313d8e273a5
imtemp <t.lin0919@gmail.com>
2022-10-18 22:29:43 -0400

    implemented tesseract

--- src/commands/flamescore.js
+++ src/commands/flamescore.js
@@ -1,9 +1,24 @@
-const { SlashCommandBuilder } = require("discord.js");
-const fetch = require("node-fetch");
+const { SlashCommandBuilder, InteractionCollector } = require("discord.js");
+const Tesseract = require("tesseract.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("flamescore")
-    .setDescription("Calculates the flamescore of an item in your screenshot"),
-  async execute(interaction) {},
+    .setDescription("Calculates the flamescore of an item in your screenshot")
+    .addStringOption((option) =>
+      option
+        .setName("image")
+        .setDescription("Link to an image of equipment")
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const image = interaction.options.getString("image");
+    console.log(image);
+    Tesseract.recognize(image, "eng", {
+      logger: (m) => console.log(m),
+    }).then(({ data: { text } }) => {
+      console.log(text);
+    });
+    await interaction.reply("a");
+  },
 };
